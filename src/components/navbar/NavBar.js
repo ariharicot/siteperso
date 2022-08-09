ba91ee28a38a9b801c0a2b46eb859df5fb1bfad5
Bredon <arileo.pereira@gmail.com>
2022-08-09 15:58:05 +0200

    navbarcontinues

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './navbar.css'
 
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  // for changing the bg when scrolling down
+  const [scrolled, setScrolled] = useState(false);
+
+  // function for what happens when we scroll
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+    window.addEventListener("scroll", onScroll);
+    // since we use addEventListener, we need to remove it once the component gets removed from the dom
+    return () => window.removeEventListener("scroll", onScroll)
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">Alvaronne</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -15,19 +38,14 @@ export const NavBar = () => {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            {/* activeLink for enlightening the active link on the navbar */}
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
-          <span className="navbar-text">
-            <div className="social-container">
-            <GitHubIcon href="https://github.com/ariharicot"/>
-            <LinkedInIcon href="https://www.linkedin.com/in/ariana-bredon-b55008217/" />
-            </div>
             <button className="contact" onClick={() => console.log("connect")}>
               <span>Envoyer un message</span>
             </button>
-          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
