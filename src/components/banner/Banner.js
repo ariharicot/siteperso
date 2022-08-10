178764a0634a89c36f7e9a1dbd012ba795606eb8
Bredon <arileo.pereira@gmail.com>
2022-08-10 11:18:23 +0200

    bannerstyling

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import plume from "../assets/plume.png";
+import './banner.css'
 
 const Banner = () => {
+
     return (
         <section className='banner' id='home'>
             <Container>
@@ -12,14 +13,13 @@ const Banner = () => {
                         <span className="tagline">
                             Bienvenu(e) sur mon portfolio
                         </span>
-                        <h1>{`Je suis Ariana Bredon`}
-                            <span className="wrap">Développeur Web</span>
+                        <h1 className="banner-title">Je suis Ariana BREDON, Développeur Web
                         </h1>
                         <p>Ea anim veniam ea duis sunt cupidatat esse pariatur aliqua ad laboris. Amet consectetur mollit in dolor ullamco sunt proident cillum eiusmod esse amet exercitation irure sint. Laboris occaecat dolor anim ut amet adipisicing velit officia aliquip excepteur non laborum. Sunt proident est cupidatat fugiat. Dolor qui anim sunt exercitation tempor. Officia culpa laboris esse tempor cillum commodo duis.</p>
-                        <button onClick={() => console.log()}>Me Contacter <ArrowCircleRightIcon /></button>
+                        <button onClick={() => console.log()}>Me Contacter <ArrowCircleRightIcon className='button-icon'/></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={plume} alt="photo de plume"></img>
+                        <img src='./public/assets/plume.svg' alt="dessin d'une plume"></img>
                     </Col>
                 </Row>
             </Container>
