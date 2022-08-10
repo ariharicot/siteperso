629a3699276d3f8d4246a3c3fd63f9ebb49beeeb
Bredon <arileo.pereira@gmail.com>
2022-08-10 09:46:56 +0200

    responsivenavbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { NavBar } from './components/navbar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import './navbar.css'
 
 export const NavBar = () => {
@@ -30,24 +28,28 @@ export const NavBar = () => {
   }
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+    <Navbar collapseOnSelect expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">Alvaronne</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        {/*<Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
+  </Navbar.Toggle>*/}
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* activeLink for enlightening the active link on the navbar */}
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
-            <button className="contact" onClick={() => console.log("connect")}>
-              <span>Envoyer un message</span>
-            </button>
+          <div className="contact-form">
+          <button className="contact" onClick={() => console.log("connect")}>
+            <span>Envoyer un message</span>
+          </button>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
+
